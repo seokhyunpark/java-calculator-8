b96f506f9108d7c718f5345eeb6daa2ba4056d49
skhnpark <skhnpark125@gmail.com>
2025-10-20 03:14:04 +0900

    feat: 계산기 컨트롤러 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,4 +1,21 @@
 package calculator.controller;
 
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class CalculatorController {
+    private final CalculatorService service;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CalculatorController(CalculatorService service, InputView inputView, OutputView outputView) {
+        this.service = service;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+        outputView.writeResult(service.calculate(inputView.read()));
+    }
 }
