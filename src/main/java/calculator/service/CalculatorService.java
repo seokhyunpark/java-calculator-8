f028732df8e73b6b84e20b279511fa88345e052e
skhnpark <skhnpark125@gmail.com>
2025-10-20 03:07:46 +0900

    feat: 커스텀 구분자를 추출하는 기능 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,4 +1,19 @@
 package calculator.service;
 
 public class CalculatorService {
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
+
+    private String extractDelimiter(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int endIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            if (endIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 입력 형식이 맞지 않습니다.");
+            }
+            String customDelimiter = input.substring(2, endIndex);
+            return "[,:" + customDelimiter + "]";
+        }
+        return "[,:]";
+    }
+
 }
