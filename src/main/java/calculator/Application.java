9e7611d73f9a0ab6211c814999e24b121255679f
skhnpark <skhnpark125@gmail.com>
2025-10-20 03:14:20 +0900

    feat: 애플리케이션 진입점 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculatorService service = new CalculatorService();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController controller = new CalculatorController(service, inputView, outputView);
+
+        controller.run();
     }
 }
